f465afb874e05de78a92dc5235aae35e8b03d96e
kevinMonteza <kmonteza95@gmail.com>
2019-02-08 14:57:14 -0500

    res fix json

--- routes/router.js
+++ routes/router.js
@@ -9,7 +9,7 @@ router.get('/persona',formController.getFormPersona);
 //router.post('/persona', formController.saveFormPersona);
 router.get('/woo', function(req,res){
     console.log(req.body)
-    res.json({
+    res.json([{
         ProductId:"1",
         ProductName:"asus",
         ProductUrl:"http://woo.prodequa.com/product/asus-vivobook-f510ua-fhd-laptop-intel-core-i5-8250u-8gb-ram-1tb-hdd-usb-c/"
@@ -25,7 +25,7 @@ router.get('/woo', function(req,res){
         ProductId:"4",
         ProductName:"dell",
         ProductUrl:"http://woo.prodequa.com/product/2017-acer-flagship-cb3-532-15-6-hd-premium-chromebook-intel-celeron-dual-core-n3060-hasta-2-48-ghz-2-gb-de-ram-16-gb-ssd-wireless-ac-bluetooth-hdmi-usb-3-0-camara-web-cro/" 
-    })
+    }])
 })
 router.put('/persona/:id', formController.updateFormPersona);
 router.delete('/persona/:id', formController.destroyFormPersona);
