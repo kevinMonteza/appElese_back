3f49c28e49bddd2fee3394b5d5706ac9241b2553
kevinMonteza <kmonteza95@gmail.com>
2019-02-08 11:09:44 -0500

    test woocommerce

--- routes/router.js
+++ routes/router.js
@@ -6,7 +6,15 @@ const formController = require('../controllers/formController');
 
 // rutas api form persona natural 
 router.get('/persona',formController.getFormPersona);
-router.post('/persona', formController.saveFormPersona);
+//router.post('/persona', formController.saveFormPersona);
+router.get('/woo', function(req,res){
+    console.log(req.body)
+    res.json({
+        ProductId:"1",
+        productName:"coca",
+        ProductUrl:"http://woo.prodequa.com/product/asus-vivobook-f510ua-fhd-laptop-intel-core-i5-8250u-8gb-ram-1tb-hdd-usb-c/"
+    })
+})
 router.put('/persona/:id', formController.updateFormPersona);
 router.delete('/persona/:id', formController.destroyFormPersona);
 
