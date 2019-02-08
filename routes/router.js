47ce8600e475785f9cd78e4c66574bbfb727bd0f
kevinMonteza <kmonteza95@gmail.com>
2019-02-08 16:59:13 -0500

    aaaa

--- routes/router.js
+++ routes/router.js
@@ -9,33 +9,39 @@ router.get('/persona',formController.getFormPersona);
 //router.post('/persona', formController.saveFormPersona);
 router.get('/woo', function(req,res){
     console.log(req.body)
-    res.json({"products":[ 
-        {
-            ProductId:"1",
-            ProductName:"asus",
-            ProductUrl:"http://woo.prodequa.com/product/asus-vivobook-f510ua-fhd-laptop-intel-core-i5-8250u-8gb-ram-1tb-hdd-usb-c/",
-            CategoryId:'1',
-            CategoryName:"Laptops"
-        },{
-            ProductId:"2",
-            ProductName:"Acer",
-            ProductUrl:"http://woo.prodequa.com/product/acer-aspire-e-15-15-6-full-hd-8th-gen-intel-core-i3-8130u-6gb-memoria-ram-1tb-hdd-8x-dvd-e5-576-392h/",
-            CategoryId:'1',
-            CategoryName:"Laptops"
-        },{
-            ProductId:"3",
-            ProductName:"HP",
-            ProductUrl:"http://woo.prodequa.com/product/hp-15-6-business-laptop-hd-wled-backled-touchscreen-display-intel-i3-7100u-processor-12gb-ddr4-ram-1tb-hdd-dvd-rw-802-11ac-wifi-hdmi-windows-10-negro/",
-            CategoryId:'1',
-            CategoryName:"Laptops"
-        },{
-            ProductId:"4",
-            ProductName:"dell",
-            ProductUrl:"http://woo.prodequa.com/product/2017-acer-flagship-cb3-532-15-6-hd-premium-chromebook-intel-celeron-dual-core-n3060-hasta-2-48-ghz-2-gb-de-ram-16-gb-ssd-wireless-ac-bluetooth-hdmi-usb-3-0-camara-web-cro/",
-            CategoryId:'1',
-            CategoryName:"Laptops" 
-        }    
-    ]})
+    // res.json({"products":[ 
+    //     {
+    //         ProductId:"1",
+    //         ProductName:"asus",
+    //         ProductUrl:"http://woo.prodequa.com/product/asus-vivobook-f510ua-fhd-laptop-intel-core-i5-8250u-8gb-ram-1tb-hdd-usb-c/",
+    //         CategoryId:'1',
+    //         CategoryName:"Laptops"
+    //     },{
+    //         ProductId:"2",
+    //         ProductName:"Acer",
+    //         ProductUrl:"http://woo.prodequa.com/product/acer-aspire-e-15-15-6-full-hd-8th-gen-intel-core-i3-8130u-6gb-memoria-ram-1tb-hdd-8x-dvd-e5-576-392h/",
+    //         CategoryId:'1',
+    //         CategoryName:"Laptops"
+    //     },{
+    //         ProductId:"3",
+    //         ProductName:"HP",
+    //         ProductUrl:"http://woo.prodequa.com/product/hp-15-6-business-laptop-hd-wled-backled-touchscreen-display-intel-i3-7100u-processor-12gb-ddr4-ram-1tb-hdd-dvd-rw-802-11ac-wifi-hdmi-windows-10-negro/",
+    //         CategoryId:'1',
+    //         CategoryName:"Laptops"
+    //     },{
+    //         ProductId:"4",
+    //         ProductName:"dell",
+    //         ProductUrl:"http://woo.prodequa.com/product/2017-acer-flagship-cb3-532-15-6-hd-premium-chromebook-intel-celeron-dual-core-n3060-hasta-2-48-ghz-2-gb-de-ram-16-gb-ssd-wireless-ac-bluetooth-hdmi-usb-3-0-camara-web-cro/",
+    //         CategoryId:'1',
+    //         CategoryName:"Laptops" 
+    //     }       
+    // ]})
+    res.json({
+        ProductId:"4",
+        ProductName:"dell",
+        ProductUrl:"http://woo.prodequa.com/product/2017-acer-flagship-cb3-532-15-6-hd-premium-chromebook-intel-celeron-dual-core-n3060-hasta-2-48-ghz-2-gb-de-ram-16-gb-ssd-wireless-ac-bluetooth-hdmi-usb-3-0-camara-web-cro/",
+        CategoryId:'1'
+    })
 })
 router.put('/persona/:id', formController.updateFormPersona);
 router.delete('/persona/:id', formController.destroyFormPersona);
